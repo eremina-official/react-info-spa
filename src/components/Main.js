f773fbc87c054204fdd5d065f4c5e33344a567c7
Marina Eremina <eremina.jobmail@gmail.com>
2019-02-02 18:42:12 +0100

    add Switch and NoMatch components

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import ReactHome from './ReactHome';
 import ReactInfo from './ReactInfo';
 import ReactComponents from './ReactComponents';
@@ -9,19 +9,23 @@ import CreateReactApp from './CreateReactApp';
 import ES6Modules from './ES6Modules';
 import Webpack from './Webpack';
 import ReactRouter from './ReactRouter';
+import NoMatch from './NoMatch';
 
 const Main = () => {
   return (
     <div>
-      <Route exact path="/" component={ReactHome} />
-      <Route path="/react-info" component={ReactInfo} />
-      <Route path="/react-components" component={ReactComponents} />
-      <Route path="/react-elements" component={ReactElements} />
-      <Route path="/react-jsx" component={ReactJSX} />
-      <Route path="/create-react-app" component={CreateReactApp} />
-      <Route path="/react-router" component={ReactRouter} />
-      <Route path="/es6-modules" component={ES6Modules} />
-      <Route path="/webpack" component={Webpack} />
+      <Switch>
+        <Route exact path="/" component={ReactHome} />
+        <Route path="/react-info" component={ReactInfo} />
+        <Route path="/react-components" component={ReactComponents} />
+        <Route path="/react-elements" component={ReactElements} />
+        <Route path="/react-jsx" component={ReactJSX} />
+        <Route path="/create-react-app" component={CreateReactApp} />
+        <Route path="/react-router" component={ReactRouter} />
+        <Route path="/es6-modules" component={ES6Modules} />
+        <Route path="/webpack" component={Webpack} />
+        <Route component={NoMatch} />
+      </Switch>
     </div>
   );
 };
