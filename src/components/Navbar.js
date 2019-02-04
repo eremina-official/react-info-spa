79103d00704061b1e929941a98766cd0628ebbaa
Marina Eremina <eremina.jobmail@gmail.com>
2019-02-04 22:52:56 +0100

    add react-bootstrap

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,30 +1,30 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
-const Navbar = () => {
+const Navigation = () => {
   return (
-    <div className="col-sm-4 align-items-start navbar navbar-expand-sm navbar-light bg-light pt-4">
-      <button className="navbar-toggler mb-3" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse flex-column" id="navbarSupportedContent">
-        <p className="font-weight-bold mb-0 align-self-start pl-sm-2">Contents</p>
-        <ul className="navbar-nav mr-auto flex-column list-unstyled">
+    <Navbar variant="light" bg="light" expand="md" collapseOnSelect className="align-items-start pt-4 min-height-100vh">
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav" className="flex-column">
+        <p className="font-weight-bold mb-0 align-self-start pt-3 pt-md-0 pl-md-2">Contents</p>
+        <Nav className="mr-auto flex-column">
           {/* exact attribute must be set for Navlink to="/",
           otherwise className="active" attribute is always applied */}
-          <li className="nav-item"><NavLink exact to="/" className="nav-link">Intro</NavLink></li>
-          <li className="nav-item"><NavLink to="/react-info" className="nav-link">React</NavLink></li>
-          <li className="nav-item"><NavLink to="/react-components" className="nav-link">React Components</NavLink></li>
-          <li className="nav-item"><NavLink to="/react-elements" className="nav-link">React Elements</NavLink></li>
-          <li className="nav-item"><NavLink to="/react-jsx" className="nav-link">JSX</NavLink></li>
-          <li className="nav-item"><NavLink to="/create-react-app" className="nav-link">Create React App</NavLink></li>
-          <li className="nav-item"><NavLink to="/react-router" className="nav-link">React Router</NavLink></li>
-          <li className="nav-item"><NavLink to="/es6-modules" className="nav-link">ES6 Modules</NavLink></li>
-          <li className="nav-item"><NavLink to="/webpack" className="nav-link">Webpack</NavLink></li>
-        </ul>
-      </div>
-    </div>
+          <Nav.Item><NavLink exact to="/" className="nav-link">Intro</NavLink></Nav.Item>
+          <Nav.Item><NavLink to="/react-info" className="nav-link">React</NavLink></Nav.Item>
+          <Nav.Item><NavLink to="/react-components" className="nav-link">React Components</NavLink></Nav.Item>
+          <Nav.Item><NavLink to="/react-elements" className="nav-link">React Elements</NavLink></Nav.Item>
+          <Nav.Item><NavLink to="/react-jsx" className="nav-link">JSX</NavLink></Nav.Item>
+          <Nav.Item><NavLink to="/create-react-app" className="nav-link">Create React App</NavLink></Nav.Item>
+          <Nav.Item><NavLink to="/react-router" className="nav-link">React Router</NavLink></Nav.Item>
+          <Nav.Item><NavLink to="/es6-modules" className="nav-link">ES6 Modules</NavLink></Nav.Item>
+          <Nav.Item><NavLink to="/webpack" className="nav-link">Webpack</NavLink></Nav.Item>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 };
 
-export default Navbar;
+export default Navigation;
