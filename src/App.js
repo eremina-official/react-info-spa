83ff63060ba2cc80236de9389c5f1567f8747bd7
Marina Eremina <eremina.jobmail@gmail.com>
2019-02-04 22:49:25 +0100

    add react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import Main from './components/Main';
-import Navbar from './components/Navbar';
+import Navigation from './components/Navbar';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <div className="App container">
-          <div className="row">
-            <Navbar className="col-4" />
-            <Main className="col-8" />
-          </div>
-        </div>
+        <Container className="App">
+          <Row>
+            <Col xs={12} md={4} className="bg-light">
+              <Navigation />
+            </Col>
+            <Col xs={12} md={8}>
+              <Main />
+            </Col>
+          </Row>
+        </Container>
       </BrowserRouter>
     );
   }
