52f0e970c0a6ae302e050edb8d9b4b8b16f0c222
Marina Eremina <eremina.jobmail@gmail.com>
2019-02-05 13:14:23 +0100

    create production build

---
+++ build/precache-manifest.c0db5c6e9f89b5398d8ecf32dca2f81f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "719fa7c8468d41c2b611",
+    "url": "/static/js/main.719fa7c8.chunk.js"
+  },
+  {
+    "revision": "b260573cf600a5f0c059",
+    "url": "/static/js/1.b260573c.chunk.js"
+  },
+  {
+    "revision": "719fa7c8468d41c2b611",
+    "url": "/static/css/main.0e3aeba9.chunk.css"
+  },
+  {
+    "revision": "2b950035143142896d7dc2d0f031cae2",
+    "url": "/index.html"
+  }
+];
