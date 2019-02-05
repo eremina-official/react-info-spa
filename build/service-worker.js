6abb7b3887b51bc280bc95e6dfa209548ab78d5e
Marina Eremina <eremina.jobmail@gmail.com>
2019-02-05 19:43:11 +0100

    add build files

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c0db5c6e9f89b5398d8ecf32dca2f81f.js"
+  "/eremina-official/precache-manifest.d4ecb055304f6dddb0cfdbe93778bc24.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/eremina-official/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
