44896033f7814985622ceb24600743c6b74e2346
Marina Eremina <eremina.jobmail@gmail.com>
2019-02-07 23:46:53 +0100

    further work with text

--- src/components/ReactElements.js
+++ src/components/ReactElements.js
@@ -4,10 +4,11 @@ const ReactElements = () => {
   return (
     <div>
       <h1>React elements</h1>
-      <p>Elements are the smallest building blocks of React apps.</p>
+      <h2>On this page main concepts of React Elements are presented.</h2>
+      <p><a href="https://reactjs.org/docs/rendering-elements.html">Elements</a> are the smallest building blocks of React apps.</p>
       <p>Elements are plain JavaScript objects.</p>
       <p>Elements are immutable.</p>
-      <p>The most common way to create an element is via using JSX which is compiled to React.createElement() call.</p>
+      <p>The most common way to create an element is via the JSX which is compiled to <code>React.createElement()</code> call.</p>
     </div>
   );
 };
