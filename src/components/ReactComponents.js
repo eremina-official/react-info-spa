5902070c0ad38f4f2d786791b0520d4b6f6a6a06
Marina Eremina <eremina.jobmail@gmail.com>
2019-02-07 23:46:00 +0100

    further work with text

--- src/components/ReactComponents.js
+++ src/components/ReactComponents.js
@@ -5,12 +5,12 @@ const ReactComponents = () => {
     <div>
       <h1>React components</h1>
       <h2>On this page the React Components main concepts are presented.</h2>
-      <p>React Components are independent, reusable pieces of UI.</p>
+      <p><a href="https://reactjs.org/docs/components-and-props.html">React Components</a> are independent, reusable pieces of UI.</p>
       <p>React Components return React Elements.</p>
       <p>Components are created using JavaScript function declaration or by subclassing <code>React.Component</code> class (Function and Class Components).</p>
-      <p>The <code>render()</code> method is mandatory in a <code>React.Component</code> subclass.</p>
+      <p>The <a href="https://reactjs.org/docs/react-component.html#render"><code>render()</code></a> method is mandatory in a <code>React.Component</code> subclass.</p>
       <p>What <code>render()</code> method does when called: it inspects <code>this.props</code> and <code>this.state</code> and returns an element to render or null (<a href="https://reactjs.org/docs/react-component.html#render">link</a>).</p>
-      <p><code>render()</code> should be a pure function.</p>
+      <p><code>render()</code> method should be a pure function.</p>
     </div>
   );
 };
