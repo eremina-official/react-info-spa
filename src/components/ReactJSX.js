d14664119299bf5cd4f9bc8bdf360d4dc023021c
Marina Eremina <eremina.jobmail@gmail.com>
2019-02-09 22:10:30 +0100

    further work with text

--- src/components/ReactJSX.js
+++ src/components/ReactJSX.js
@@ -3,10 +3,11 @@ import React from 'react';
 const ReactJSX = () => {
   return (
     <div>
-      <h1>JSX (JavaScript XML)</h1>
-      <p>JSX is a JavaScript syntax extention.</p>
+      <h1>JSX</h1>
+      <h2>This page describes JSX (JavaScript XML).</h2>
+      <p><a href="https://reactjs.org/docs/introducing-jsx.html">JSX</a> is a JavaScript syntax extention.</p>
       <p>JSX is used to describe how the UI should look like.</p>
-      <p>JSX is compiled to React.createElement() call that returns a React element. </p>
+      <p>JSX is compiled to <code>React.createElement()</code> call that returns a React element. </p>
     </div>
   );
 };
