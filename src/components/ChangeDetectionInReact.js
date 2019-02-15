67b4ec17060859113e378672639c509b8a7f962d
Marina Eremina <eremina.jobmail@gmail.com>
2019-02-15 14:02:06 +0100

    add ChangeDetectionInReact.js

---
+++ src/components/ChangeDetectionInReact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ChangeDetectionInReact = () => {
+  return (
+    <div>
+      <h1>Change Detection in React</h1>
+      <h2>This page is an overview of the change detection mechanism in React</h2>
+    </div>
+  );
+};
+
+export default ChangeDetectionInReact;
