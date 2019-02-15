0814e50a5a7fe79ada95b205df95f558aaad1cb6
Marina Eremina <eremina.jobmail@gmail.com>
2019-02-15 20:44:29 +0100

    further work with text

--- src/components/ChangeDetectionInReact.js
+++ src/components/ChangeDetectionInReact.js
@@ -4,7 +4,13 @@ const ChangeDetectionInReact = () => {
   return (
     <div>
       <h1>Change Detection in React</h1>
-      <h2>This page is an overview of the change detection mechanism in React</h2>
+      <h2>This page is an overview of the change detection mechanism in React.</h2>
+      <p className="font-italic font-weight-light">What is change detection?</p>
+      <p>Change detection is a mechanism that synchronises the application state with the user interface.</p>
+      <p>At its core change detection is a number of algorithms in a given framework or a library that track changes in the application state and render the updated state to the screen.</p>
+      <p>Change detection process has two main parts: tracking changes and rendering.</p>
+      <p className="font-italic font-weight-light">Change detection implementation in React</p>
+
     </div>
   );
 };
