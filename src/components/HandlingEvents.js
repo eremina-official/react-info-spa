52ba3f526d15432d8999af4926629a7e03479395
Marina Eremina <eremina.jobmail@gmail.com>
2019-02-18 21:52:50 +0100

    further work with text

--- src/components/HandlingEvents.js
+++ src/components/HandlingEvents.js
@@ -8,7 +8,7 @@ const HandlingEvents = () => {
       <p>Event handlers are declared in JSX (when an event handler is declared, under the hood React selects the relevant HTML DOM nodes and sets event listeners for them).</p>
       <p>Event handlers can be attached only to elements that represent DOM nodes and can not be attached to elements that represent user-defined components.</p>
       <p>React passes to event handlers instances of <a href="https://reactjs.org/docs/events.html"><code>SyntheticEvent</code></a>.</p>
-      <p><code>SyntheticEvent</code> is a cross-browser wrapper around the browser’s native event.</p>
+      <p><code>SyntheticEvent</code> is a cross-browser wrapper around the browser’s native event (this wrapper accounts for differences in events implementation across different browsers).</p>
     </div>
   );
 };
