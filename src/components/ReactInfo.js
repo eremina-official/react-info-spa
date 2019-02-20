9abb99d66a2258dd1bfe5f68fd98ebf223178fd4
Marina Eremina <eremina.jobmail@gmail.com>
2019-02-20 22:50:42 +0100

    further work with text

--- src/components/ReactInfo.js
+++ src/components/ReactInfo.js
@@ -9,6 +9,7 @@ const ReactInfo = () => {
       <p>React offers a new way of constructing the UI: it supports <a href="https://reactjs.org/docs/introducing-jsx.html#why-jsx">separation of concerns</a> instead of separaion of technologies.</p>
       <p>React enforces declarative programming (<a href="https://reactjs.org/docs/faq-internals.html#what-is-the-virtual-dom">link</a>).</p>
       <p>As a part of a declarative approach React abstracts away HTML DOM manipulation: we declare what a component should look like and React calls the HTML DOM API methods under the hood (<a href="https://reactjs.org/docs/faq-internals.html#what-is-the-virtual-dom">link</a>).</p>
+      <p>It's considered the best practice not to manipulate the HTML DOM directly when using React.</p>
     </div>
   );
 };
