c160dbb21a4b0aecc57193a039fe95a67e1db2f3
Marina Eremina <eremina.jobmail@gmail.com>
2019-02-21 19:16:17 +0100

    add setState description

--- src/components/ReactComponents.js
+++ src/components/ReactComponents.js
@@ -13,11 +13,21 @@ const ReactComponents = () => {
       <p className="font-italic font-weight-bold">State</p>
       <p>State is a feature available only for Class Components (not available for Function Components).</p>
       <p>State is a JavaScript object.</p>
-      <p>Initial component state is assigned directly in a <code>constructor()</code> method.</p>
-      <p>When the state should be changed, it is not changed directly (by an assignment) but with a <code>setState()</code> function call.</p>
+      <p>Initial component state (which is used for component mounting) is assigned directly in a <code>constructor()</code> method.</p>
+      <p>When the state should be changed, it is not changed directly (by an assignment of <code>this.state</code>) but with a <code>setState()</code> function call.</p>
 
       <p className="font-italic font-weight-bold">Props</p>
       <p>Props is a JavaScript object.</p>
+
+      <p className="font-italic font-weight-bold">setState()</p>
+      <p><code>setState</code> does not always update <code>this.state</code> immediately after it is called, sometimes multiple <code>setState</code> calls can be batched and executed later (<a href="https://reactjs.org/docs/state-and-lifecycle.html#state-updates-may-be-asynchronous">link</a>).</p>
+      <p><code>setState()</code> can take two kinds of parameters: an object literal or a function.</p>
+      <p>The object literal parameter is used when the timing of state update is not important for app functioning (means that next application state does not rely on the updated state value).</p>
+      <p>The function parameter is used when the timing of setting a new state matters. The function passed to the <code>setState()</code> receives previous state and props at the time update is applied as arguments. This function should be pure.</p>
+
+      
+      <p className="font-weight-bold">Component Lifecycle Methods</p>
+      <p>Lifecycle methods is a feature available only for Class Components (not available for Function Components).</p>
 
       <p className="font-italic font-weight-bold">Render()</p>
       <p>The <a href="https://reactjs.org/docs/react-component.html#render"><code>render()</code></a> method is mandatory in a <code>React.Component</code> subclass.</p>
