66087ef0912b96e24d2a0915df14e852798ecb9f
Marina Eremina <eremina.jobmail@gmail.com>
2019-02-26 13:01:21 +0100

    add links to external resources

--- src/components/ChangeDetectionInReact.js
+++ src/components/ChangeDetectionInReact.js
@@ -4,7 +4,8 @@ const ChangeDetectionInReact = () => {
   return (
     <div>
       <h1>Change Detection in React</h1>
-      <h2>This page is an overview of the change detection mechanism in React.</h2>
+      <h2>This page is an overview of the change detection mechanism in <span className="font-weight-bold">React v16</span>. The <a href="https://twitter.com/dan_abramov/status/981712092611989509?lang=en">Render and Commit phases</a> of the component lifecycle are described for the web apps that use React DOM <a href="https://overreacted.io/react-as-a-ui-runtime/#renderers">renderer</a>.</h2>
+
       <p className="font-italic font-weight-bold">What is change detection?</p>
       <p>Change detection is a mechanism that synchronises the application state with the user interface.</p>
       <p>At its core change detection is a number of algorithms in a given framework or a library that track changes in the application state and render the updated state to the screen.</p>
@@ -12,7 +13,9 @@ const ChangeDetectionInReact = () => {
 
       <p className="font-italic font-weight-bold">Change detection implementation in React</p>
       <p>The change detection process starts with a call of <code>setState()</code> function or with a change of props.</p>
-      <p>After the change detection process is started, React executes the <code>render()</code> function that returns a new version of Virtual DOM tree. React compares the old and the new Virtual DOM to find difference between them. When the difference is found, React creates a patch that updates the corresponding HTML DOM nodes.</p>
+      <p>After the change detection process is started, React executes the <code>render()</code> function that returns a new version of Virtual DOM tree. React compares the old and the new Virtual DOM to find difference between them (<a href="https://reactjs.org/docs/reconciliation.html">reconciliation</a>). When the difference is found, React creates a patch that is used to update the corresponding HTML DOM nodes.</p>
+
+      <p>React updates the HTML DOM during the <a href="https://twitter.com/dan_abramov/status/981712092611989509?lang=en">Commit phase</a>.</p>
     </div>
   );
 };
