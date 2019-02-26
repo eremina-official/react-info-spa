41c8d58fa1d255c64e4c54369246cde77a1901c0
Marina Eremina <eremina.jobmail@gmail.com>
2019-02-26 13:03:02 +0100

    further work with text

--- src/components/ReactElements.js
+++ src/components/ReactElements.js
@@ -9,7 +9,7 @@ const ReactElements = () => {
       <p>The most common way to create an element is via the JSX which is compiled to <code>React.createElement()</code> call.</p>
       <p><code>React.createElement()</code> function creates a React Element - a plain Javascript object that describes some HTML element (its properties and children).</p>
       <p>All created React Elements are included as nodes into React <a href="https://reactjs.org/docs/faq-internals.html#what-is-the-virtual-dom">Virtual DOM</a>.</p>
-      <p>Elements are <a href="https://reactjs.org/docs/rendering-elements.html#updating-the-rendered-element">immutable</a>, Elements' immutability is one of the most important features required for Virtual DOM comparisons in the Change Detection process.</p>
+      <p>Elements are <a href="https://reactjs.org/docs/rendering-elements.html#updating-the-rendered-element">immutable</a>, Elements' immutability is one of the most important features required for the Virtual DOM comparisons in the Change Detection process.</p>
       <p>The <code>ReactDOM.render()</code> method renders React Elements into the HTML DOM nodes.</p>
     </div>
   );
