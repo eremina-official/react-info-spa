359df48376afdf6f150472fa3f6f659069fd91b9
Marina Eremina <eremina.jobmail@gmail.com>
2019-02-26 13:04:06 +0100

    further work with text

--- src/components/ReactJSX.js
+++ src/components/ReactJSX.js
@@ -7,7 +7,8 @@ const ReactJSX = () => {
       <h2>This page describes JSX (JavaScript XML).</h2>
       <p><a href="https://reactjs.org/docs/introducing-jsx.html">JSX</a> is a JavaScript syntax extention.</p>
       <p>JSX is used to describe how the UI should look like.</p>
-      <p>JSX is compiled to <code>React.createElement()</code> call that returns a React element. </p>
+      <p>JSX is compiled by <a href="https://babeljs.io/">Babel</a> to <code>React.createElement()</code> call that returns a React Element. </p>
+      <p>Any valid JavaScript expression can be embedded inside the curly braces in JSX.</p>
     </div>
   );
 };
