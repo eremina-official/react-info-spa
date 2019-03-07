55addd50990ac22db2ea3f076cf0979f3435c292
Marina Eremina <eremina.jobmail@gmail.com>
2019-03-07 19:19:53 +0100

    add links to external resources

--- src/components/ES6Modules.js
+++ src/components/ES6Modules.js
@@ -10,7 +10,7 @@ const ES6Modules = () => {
       <p>For a browser to know that a file is a ES6 module the script tag must have a type="module" attribute.</p>
       <p>Each module has its own scope (module scope).</p>
       <p>Modules are in the strict mode by default.</p>
-      <p>Modules are deferred by default (modules are loaded like if the defer attribute is set on the script tag).</p>
+      <p>Modules are deferred by default, it means that modules are loaded like if the defer attribute is set on the script tag (<a href="https://developers.google.com/web/fundamentals/primers/modules#defer">link</a>).</p>
       <p>Modules are fetched with CORS (as for 01.2019).</p>
       <p>Import and export declarations are syntactically restricted to the top-level scope in the module body (that means it should be no import/export declarations inside the block statements or functions).</p>
       <p>ES6 modules use live bindings (that means both import and export point to the same location in memory, if the exporting module changes a value, that value will be changed in the importing module as well).</p>
