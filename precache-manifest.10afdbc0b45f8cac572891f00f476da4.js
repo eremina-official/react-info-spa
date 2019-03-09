948e7d022293ad122ea3b94bbd4e082753f24371
Marina Eremina <eremina.jobmail@gmail.com>
2019-03-09 20:07:04 +0100

    Updates

--- precache-manifest.10afdbc0b45f8cac572891f00f476da4.js
+++ precache-manifest.10afdbc0b45f8cac572891f00f476da4.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/react-info-spa/static/js/runtime~main.888598db.js"
   },
   {
-    "revision": "8c0eec7e05c0e33e2162",
-    "url": "/react-info-spa/static/js/main.8c0eec7e.chunk.js"
+    "revision": "21ce43a7e5126819fd0e",
+    "url": "/react-info-spa/static/js/main.21ce43a7.chunk.js"
   },
   {
     "revision": "401be0231c522e2dda54",
     "url": "/react-info-spa/static/js/1.401be023.chunk.js"
   },
   {
-    "revision": "8c0eec7e05c0e33e2162",
+    "revision": "21ce43a7e5126819fd0e",
     "url": "/react-info-spa/static/css/main.38eaeb94.chunk.css"
   },
   {
-    "revision": "2715ed25fb00ea69a8eeb17a8aa89766",
+    "revision": "2d32ba0b7eec6902b9bb344021783360",
     "url": "/react-info-spa/index.html"
   }
 ];
