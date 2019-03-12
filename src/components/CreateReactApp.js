b1cdbdd7efcba878d763a9127721e6f0a8363a93
Marina Eremina <eremina.jobmail@gmail.com>
2019-03-12 18:41:55 +0100

    further work with text

--- src/components/CreateReactApp.js
+++ src/components/CreateReactApp.js
@@ -6,8 +6,7 @@ const CreateReactApp = () => {
       <h1>Create React App</h1>
       <h2>Intro to Create React App.</h2>
       <p><a href="https://github.com/facebook/create-react-app">Create React App</a> is an officially supported CLI tool for creating React applications.</p>
-      <p>CRA offers a project setup that needs no configuration.</p>
-      <p>CRA uses project build tools (such as Webpack, Babel, ESLint) which are preconfigured by default.</p>
+      <p>CRA offers a project setup and provides project build tools (such as Webpack, Babel, ESLint) which are preconfigured by default.</p>
       <p>The project setup supports ES6 modules.</p>
       <p>For the project to build two files are mandatory: <code>public/index.html</code> (the page template) and <code>src/index.js</code> (JavaScript entry point).</p>
       <p>Only files inside the <code>src</code> directory are processed by Webpack, therefore all JS and CSS files must be placed there.</p>
