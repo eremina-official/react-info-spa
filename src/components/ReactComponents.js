4197a6cfd44c4638f9866afea6b4684713181f0c
Marina Eremina <eremina.jobmail@gmail.com>
2019-03-17 15:36:18 +0100

    edit links to external resources

--- src/components/ReactComponents.js
+++ src/components/ReactComponents.js
@@ -19,7 +19,7 @@ const ReactComponents = () => {
 
       <p className="font-italic font-weight-bold">setState()</p>
       <p><code>setState()</code> does not always update <code>this.state</code> immediately after it is called, sometimes multiple <code>setState()</code> calls can be batched and executed later (<a href="https://reactjs.org/docs/state-and-lifecycle.html#state-updates-may-be-asynchronous">link</a>).</p>
-      <p><code>setState()</code> can take two kinds of parameters: an object literal or a function.</p>
+      <p><code>setState()</code> can take two kinds of parameters: an object literal or a function (<a href="https://reactjs.org/docs/react-component.html#setstate">link</a>).</p>
       <p>The object literal parameter is used when the timing of state update is not important for app functioning (means that next application state does not rely on the updated state value).</p>
       <p>The function parameter is used when the timing of setting a new state matters. The function passed to the <code>setState()</code> receives previous state and props at the time update is applied as arguments (<a href="https://reactjs.org/docs/faq-state.html#how-do-i-update-state-with-values-that-depend-on-the-current-state">link</a>). This function should be pure (<a href="https://reactjs.org/docs/react-component.html#setstate">link</a>).</p>
 
@@ -28,7 +28,7 @@ const ReactComponents = () => {
       <p>When the state should be changed, it is not changed directly (by an assignment of <code>this.state</code>) but with a <code>setState()</code> function call (<a href="https://reactjs.org/tutorial/tutorial.html#why-immutability-is-important">link</a>).</p>
       <p>To avoid direct state change, the <code>this.state</code> properties are copied into separate varialbes, the variables are changed and then used to set a new state in a <code>setState()</code> function call.</p>
       <p>Care should be taken if the state properties are copied with methods which return a shallow copy of objects (f.e. the <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice">slice</a> method). If the object properties of <code>this.state</code> are copied by reference, then changing them in the copied object will also change them in <code>this.state</code>.</p>
-      <p>According to React <a href="https://reactjs.org/docs/react-component.html#constructor">Docs</a> it should be avoided to copy props into the state in the constructor method. Setting initial state from props is possible only when it is explicitly defined that the prop updates won't be important for the component.</p>
+      <p>According to React Docs it should be avoided to copy props into the state in the constructor method. Setting initial state from props is possible only when it is explicitly defined that the prop updates won't be important for the component (<a href="https://reactjs.org/docs/react-component.html#constructor">link</a>).</p>
 
       
       <p className="font-weight-bold pt-3">Component Lifecycle Methods</p>
