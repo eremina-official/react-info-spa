6dfd90553a080d49ca89d40c7d578a8cdb0e86b9
Marina Eremina <eremina.jobmail@gmail.com>
2019-03-17 15:36:41 +0100

    edit links to external resources

--- src/components/ReactInfo.js
+++ src/components/ReactInfo.js
@@ -10,7 +10,7 @@ const ReactInfo = () => {
       <p>React enforces declarative programming (<a href="https://reactjs.org/docs/faq-internals.html#what-is-the-virtual-dom">link</a>).</p>
       <p>As a part of a declarative approach React abstracts away HTML DOM manipulation: we declare what a component should look like and React calls the HTML DOM API methods under the hood (<a href="https://reactjs.org/docs/faq-internals.html#what-is-the-virtual-dom">link</a>).</p>
       <p>It's considered the best practice not to manipulate the HTML DOM directly when using React.</p>
-      <p>React supports one-way data binding (it means data flow one way - from owner to child). Two-way data binding can be implemented in React by listening to a "change" event, reading from the user input and updating the app state with setState() method (<a href="https://reactjs.org/docs/two-way-binding-helpers.html#overview">link</a>).</p>
+      <p>React supports <a href="https://reactjs.org/docs/two-way-binding-helpers.html#overview">one-way data binding</a> (it means data flow one way - from owner to child). Two-way data binding can be implemented in React by listening to a "change" event, reading from the user input and updating the app state with setState() method (<a href="https://reactjs.org/docs/two-way-binding-helpers.html#overview">link</a>).</p>
     </div>
   );
 };
